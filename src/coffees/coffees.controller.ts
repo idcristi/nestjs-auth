c9d302cf0aeed57b79d7592489018d2cca4e9700
idcristi <cristi.duluta@gmail.com>
2023-07-26 14:58:43 +0200

    Implement example Claims-based Authorization

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -14,12 +14,15 @@ import { ActiveUser } from 'src/iam/decorators/active-user.decorator';
 import { ActiveUserData } from 'src/iam/interfaces/active-user-data.interface';
 import { Roles } from 'src/iam/authorization/decorators/roles.decorator';
 import { Role } from 'src/users/enums/role.enums';
+import { Permissions } from 'src/iam/authorization/decorators/permissions.decorator';
+import { Permission } from '../iam/authorization/permission.type';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
-  @Roles(Role.Admin)
+  // @Roles(Role.Admin)
+  @Permissions(Permission.CreateCoffee)
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
     return this.coffeesService.create(createCoffeeDto);
